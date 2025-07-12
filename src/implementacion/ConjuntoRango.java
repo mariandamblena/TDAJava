da75db48103d37d9848faee69f499cc577f6799a
mariandamblena <marianodambo@hotmail.com>
2025-07-12 00:42:11 -0300

    Repaso ABB

--- src/implementacion/ConjuntoRango.java
+++ src/implementacion/ConjuntoRango.java
@@ -1,7 +1,41 @@
 package implementacion;
 
 import tda.ConjuntoRangoTDA;
+import tda.ConjuntoTDA;
+
+import java.util.Random;
 
 public class ConjuntoRango implements ConjuntoRangoTDA {
 
+
+    class nodo {
+        int valor;
+        nodo siguiente;
+    }
+
+    private nodo primero;
+    private int cantidad;
+    private Random r;
+
+
+    @Override
+    public void inicializarConjunto(int limiteInferior, int limiteSuperior) {
+
+
+    }
+
+    @Override
+    public void agregar(int x) {
+
+    }
+
+    @Override
+    public ConjuntoTDA obtenerEnRango() {
+        return null;
+    }
+
+    @Override
+    public ConjuntoTDA obtenerFueraDeRango() {
+        return null;
+    }
 }
